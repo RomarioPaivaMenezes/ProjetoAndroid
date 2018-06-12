13905dd4ace35725a38869af37ff2047b7960394
Romário Menezes <Romrio Menezes@DESKTOP-3VD99CC.Home>
2018-06-12 01:53:09 -0300

    Alteração Aluno get Set

--- AlunoAndroiRomariApi/src/main/java/br/com/fiap/romario/alunocloud/model/Aluno.java
+++ AlunoAndroiRomariApi/src/main/java/br/com/fiap/romario/alunocloud/model/Aluno.java
@@ -6,16 +6,26 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class Aluno {
 
-    @Id
-    private String rg;
-  	private String nome;
-  	private String dataNascimento;
-  	
+	@Id
+	private String rg;
+	private String cpf;
+	private String nome;
+	private String telefone;
+	private String dataNascimento;
+	private String email;
+	private String senha;
+	
 	public String getRg() {
 		return rg;
 	}
 	public void setRg(String rg) {
 		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
 	}
 	public String getNome() {
 		return nome;
@@ -23,12 +33,31 @@ public class Aluno {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 	public String getDataNascimento() {
 		return dataNascimento;
 	}
 	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
-
+	
+	
     
 }
